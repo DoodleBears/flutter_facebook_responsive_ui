c1d9095b2111324afef5a358d22e0fa7ffdab79e
DoodleBears <yangmufeng233@gmail.com>
2021-03-15 22:01:13 +0800

    add stories row

--- lib/widgets/circle_button.dart
+++ lib/widgets/circle_button.dart
@@ -15,16 +15,17 @@ class CircleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.all(6.0),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          shape: BoxShape.circle,
-        ),
-        child: IconButton(
-          icon: Icon(icon),
-          iconSize: iconSize,
-          color: Colors.black,
-          onPressed: onPressed,
-        ));
+      margin: const EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        icon: Icon(icon),
+        iconSize: iconSize,
+        color: Colors.black,
+        onPressed: onPressed,
+      ),
+    );
   }
 }
